cbf75ee25f350cf68ae89be94a45827859683bb0
Robson <robsondejesus1996@hotmail.com>
2019-05-15 15:36:41 -0300

    Classes funcionário e departamento com os seus atributos adicionados e métodos assessores

---
+++ br.com.udesc.crudfuncionarios/src/Funcionario.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Robson de Jesus
+ */
+public class Funcionario {
+    
+    private int matricula;
+    private String nome;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(int matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+}
